78d39609313ac549df9cd03389ffe34892320a09
Umutcan Alpak <umutcanalpak@gmail.com>
2021-01-22 23:14:44 +0300

    payment detail model

--- PaymentAPI/Models/PaymentDetail.cs
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,19 @@ namespace PaymentAPI.Models
 {
     public class PaymentDetail
     {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+
+        [Column(TypeName = "nvarchar(116)")]
+        public string CardOwnerNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; }
     }
 }
